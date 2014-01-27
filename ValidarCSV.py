69c69f23d1f5e357a3b59509d56c7bc43a47015f
SalvaJ€ <salvajgb@gmail.com>
2014-01-27 18:04:39 +0100

    verificación columna ACTIVO en encabezado y que al menos hay otra columna adicional

--- ValidarCSV.py
+++ ValidarCSV.py
@@ -6,42 +6,17 @@
 # Validador para la entrada de datos por fichero CSV al portal Casaktua
 
 
-import csv, sys, os, time, logging
+import csv
+# import sys
+# import os
+import time
+import logging
 # import win32com.client    # Libreria para iMacros
-
-
-# Generar nombre de fichero con fecha y hora
-# Formato: NOMBRE_AAAAMDHm.ext
-# AAAA año
-# M mes, enero es 1, no 01
-# D día, 3, no 03
-# H hora
-# m minutos
-# Args:
-#       prefijo: (str), nombre inicial del fichero
-#       extension: (str), extensión del fichero con el punto (ej. '.log'). Default '.txt'
-# Returns:
-#       nombreFichero: (str), con formato prefijo_AAAAMDHm.ext
-def NombrarFicheroHora(prefijo, extension='.txt'):
-    """
-    :type prefijo: str
-    :type extension: str
-    """
-    nombre = prefijo
-    tiempo = time.localtime(time.time())	# Objeto con atributos del día y la hora
-    diaHora = str(tiempo.tm_year)+str(tiempo.tm_mon)+str(tiempo.tm_mday)+str(tiempo.tm_hour)+str(tiempo.tm_min)
-    nombreFichero = nombre + '_' + diaHora + extension
-    return nombreFichero
-
-
-# Inicia el log a nivel de DEBUG (habrá que cambiarlo a INFO en producción)
-nombreFicheroLog = NombrarFicheroHora('resultado', '.log')
-logging.basicConfig(filename=nombreFicheroLog, level=logging.DEBUG)
 
 # os.chdir('c:/Python33/learning')
 
-# TODO La carga de constantes debería realizarla a traves de un modulo aparte.(o por proceso de carga de fichero setup)
 
+# TODO La carga de constantes debería realizarla a traves de un modulo aparte.(o por proceso de carga de fichero setup)
 # Definicion de constantes e inicializacion de variables globales
 ordenEncabezado = ['NULO',
                    'ACTIVO',
@@ -60,14 +35,43 @@ numeroEncabezados = len(ordenEncabezado)
 matrizEncabezados = []
 
 
+# Generar nombre de fichero con fecha y hora
+# Formato: NOMBRE_AAAAMDHm.ext
+# AAAA año
+# M mes, enero es 1, no 01
+# D día, 3, no 03
+# H hora
+# m minutos
+# Args:
+#       prefijo: (str), nombre inicial del fichero
+#       extension: (str), extensión del fichero con el punto (ej. '.log'). Default '.txt'
+# Returns:
+#       nombreFichero: (str), con formato prefijo_AAAAMDHm.ext
+def NombrarFicheroHora(prefijo, extension='.txt'):
+    """
+    :param prefijo: str
+    :param extension: str
+    :return nombre_fichero: str
+    """
+    nombre = prefijo
+    tiempo = time.localtime(time.time())    # Objeto con atributos del día y la hora
+    dia_hora = str(tiempo.tm_year)+str(tiempo.tm_mon)+str(tiempo.tm_mday)+str(tiempo.tm_hour)+str(tiempo.tm_min)
+    nombre_fichero = nombre + '_' + dia_hora + extension
+    return nombre_fichero
+
+
 # Funcion AbrirFicheroCSV()
 #   Abre el fichero 'datos.csv'
 def AbrirFicheroCSV():
+    """
+    :return: :raise:
+    """
     global ficheroEntrada       # global para poder cerrarlo luego
     try:
         ficheroEntrada = open('datos.csv','r')
     except:
         logging.error("Error al intentar abrir fichero: 'datos.csv")
+        # TODO esto estaría mejor con decoradores de funciones.
         raise
     lectorFicheroEntrada = csv.reader(ficheroEntrada, delimiter=';')
     return lectorFicheroEntrada
@@ -76,10 +80,14 @@ def AbrirFicheroCSV():
 # Funcion CerrarCSV()
 #   Cierra el fichero 'datos.csv'
 def CerrarFicheroCSV():
+    """
+    :return: :raise: 
+    """
     try:
         ficheroEntrada.close()
     except:
         logging.error("Error al intentar cerrar fichero: 'datos.csv")
+        # TODO esto estaría mejor con decoradores de funciones.
         raise
     return
 
@@ -87,6 +95,10 @@ def CerrarFicheroCSV():
 # Funcion ExtraeEncabezados()
 #   obtiene la primera fila del fichero CSV
 def ExtraeEncabezados(lectorFicheroEntrada):
+    """
+    :param lectorFicheroEntrada: file
+    :return encabezado: str
+    """
     for encabezado in lectorFicheroEntrada:
         # print(encabezado)
         break
@@ -96,12 +108,25 @@ def ExtraeEncabezados(lectorFicheroEntrada):
 
 # Validar los nombres de encabezados
 def ValidarEncabezados(encabezado):
+    """
+    :param encabezado: str
+    :return isOK: boolean
+    """
     isOK = True
+    if ordenEncabezado[1] not in encabezado:
+        logging.warning('No hay ninguna columna llamada ACTIVO, (es obligatoria)')
+        isOK = False
+        return isOK
+    elif len(list(encabezado)) < 2:
+        logging.warning('Se necesitan al menos 2 columnas (ACTIVO y otra)')
+        isOK = False
+        return isOK
     for campo in encabezado:
         if campo in ordenEncabezado:
             logging.info("%s -> OK", campo)
         else:
             logging.warning("%s Título de columna INCORRECTO!!", campo)
+            # TODO esto estaría mejor con decoradores de funciones.
             isOK = False
     return isOK
 
@@ -127,13 +152,16 @@ def IniciarMatriz():
 # Para ver si es __main__
 print(__name__)
 
+# Inicia el log a nivel de DEBUG (habrá que cambiarlo a INFO en producción)
+nombreFicheroLog = NombrarFicheroHora('resultado', '.log')
+logging.basicConfig(filename=nombreFicheroLog, encoding="utf-8", level=logging.DEBUG)
+
 # Lee primera fila del CSV y la carga en 'encabezado'
 encabezado = ExtraeEncabezados(AbrirFicheroCSV())
 CerrarFicheroCSV()
 
-
 if ValidarEncabezados(encabezado):
-    logging.info('FASE FASE VALIDACIÓN TITULOS DE COLUMNAS DE DATOS -> OK')
+    logging.info(u'FASE FASE VALIDACIÓN TITULOS DE COLUMNAS DE DATOS -> OK')
     # Llama a la funcion cargadora de la matriz de posiciones.
     matrizEncabezados = IniciarMatriz()
     # Imprime el contenido de la matriz de posiciones.
@@ -148,4 +176,4 @@ if ValidarEncabezados(encabezado):
                   linea[int(matrizEncabezados[numeroColumna][2])])      # Dato de la columna
 else:
     logging.warning('FASE VALIDACIÓN TITULOS DE COLUMNAS DE DATOS NO SUPERADA. compruebe los nombres de los títulos de las columnas en datos.csv)')
-
+    # TODO esto estaría mejor con decoradores de funciones.
